aeafafacc70116083fa0629123a4d1b1fa93b1db
Banshiks <foleg730@gmail.com>
2026-05-26 09:06:21 +0300

    Add capacitor.config.js

---
+++ capacitor.config.js
@@ -0,0 +1,13 @@
+const config = {
+  appId: 'com.berd.vampireroguelike',
+  appName: 'Vampire Roguelike',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  android: {
+    allowMixedContent: true
+  }
+};
+
+module.exports = config;
