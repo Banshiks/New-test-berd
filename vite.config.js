f8f48349a23fdf921af8c83e837c67f7f5bde68d
Banshiks <foleg730@gmail.com>
2026-05-26 09:06:19 +0300

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/ws': {
+        target: 'ws://localhost:4000',
+        ws: true
+      }
+    }
+  }
+});
