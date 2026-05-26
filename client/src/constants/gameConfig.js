540c89c18d5faaf4ce9e8cbf13f39ced5cb71722
Banshiks <foleg730@gmail.com>
2026-05-26 08:56:00 +0300

    Add client/src/constants/gameConfig.js

---
+++ client/src/constants/gameConfig.js
@@ -0,0 +1,8 @@
+export const CANVAS_WIDTH = 800;
+export const CANVAS_HEIGHT = 500;
+export const GRAVITY = 0.55;
+export const JUMP_VELOCITY = -13;
+export const PLAYER_SPEED = 4;
+export const PLAYER_WIDTH = 30;
+export const PLAYER_HEIGHT = 46;
+export const SEND_INTERVAL = 50; // ms between position updates
